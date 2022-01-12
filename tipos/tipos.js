3d76eba5b4ee1b7ce25aac938a360a9b0178d031
Bruno Henrique <b.henrique261991@gmail.com>
2022-01-12 20:35:32 -0200

    Union Types

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -12,5 +12,21 @@ var funcionario = {
         }
     }
 };
+var funcionario2 = {
+    supervisores: ['Carlos', 'Felipe'],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return 'Ponto Normal';
+        }
+        else {
+            return 'Fora do Horario';
+        }
+    }
+};
 console.log(funcionario.supervisores);
 console.log(funcionario.baterPonto(8));
+// Union Types
+var nota = 10;
+console.log("Minha nota \u00E9 " + nota);
+nota = '10';
+console.log("Minha nota \u00E9 " + nota);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,10 +1,10 @@
 let nome = 'jõao'
 console.log(nome)
 
-let funcionario: { 
-    supervisores: string[]
-    baterPonto: (hora: number) => string 
-} = {
+type funcionario = {supervisores: string[]
+    baterPonto: (hora: number) => string }
+
+let funcionario: funcionario = {
     supervisores: ['Bruno', 'Jhow'],
     baterPonto: (horario: number): string => {
         if(horario <= 8) {
@@ -14,5 +14,23 @@ let funcionario: {
             }
         }
     }
+
+    let funcionario2: funcionario = {
+        supervisores: ['Carlos', 'Felipe'],
+        baterPonto: (horario: number): string=> {
+            if(horario <= 8) {
+                return 'Ponto Normal' }
+                else {
+                   return 'Fora do Horario'
+                }
+            }
+        }
 console.log(funcionario.supervisores)
 console.log(funcionario.baterPonto(8))
+
+// Union Types
+
+let nota: number | string = 10
+console.log(`Minha nota é ${nota}`)
+nota = '10'
+console.log(`Minha nota é ${nota}`)
