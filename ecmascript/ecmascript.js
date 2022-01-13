b34fda7afe340e2fb8f3531ba018485cad2fccab
Bruno Henrique <b.henrique261991@gmail.com>
2022-01-13 11:13:18 -0200

    Paramentros Padrão

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -7,4 +7,16 @@ console.log(somar(2, 2));
 //Arrow Function
 var subtrair = function (n1, n2) { return n1 - n2; };
 console.log(subtrair(4, 3));
+// Parametros Padrão
+function contagemRegressiva(inicio, fim) {
+    if (inicio === void 0) { inicio = 5; }
+    if (fim === void 0) { fim = inicio - 5; }
+    console.log(inicio);
+    while (inicio > fim)
+        inicio--;
+    console.log(inicio);
+}
+console.log('Fim');
+contagemRegressiva();
+contagemRegressiva(3);
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -8,3 +8,18 @@ console.log(somar(2,2))
 //Arrow Function
 const subtrair = (n1:number, n2: number) => n1 - n2
 console.log(subtrair(4,3))
+
+
+// Parametros Padrão
+
+function contagemRegressiva(inicio: number = 5, 
+    fim: number = inicio -5) : void {
+        console.log(inicio);
+        while(inicio > fim)
+        inicio--
+        console.log(inicio)
+    }
+    console.log('Fim')
+
+    contagemRegressiva()
+    contagemRegressiva(3)
