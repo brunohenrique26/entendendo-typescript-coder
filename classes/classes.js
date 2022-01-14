f240c33acde883f592bebb398a2e88da86fb2c37
Bruno Henrique <b.henrique261991@gmail.com>
2022-01-14 17:04:36 -0200

    Henrança

--- classes/classes.js
+++ classes/classes.js
@@ -35,4 +35,41 @@ const celular = new Produto('iphone', 100);
 const celular2 = new Produto('Samsung', 10, 0);
 celular2.desconto = 0.06;
 console.log(celular2.resumo());
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0
+            && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('For', 'Ka', 185);
+// console.log(carro1.acelerar())
+// console.log(carro1.acelerar())
+// console.log(carro1.acelerar())
+// Henrança
+class Ferrari extends Carro {
+}
+const f40 = new Ferrari('ferrari', 'f40', 324);
+console.log(`${f40.marca} ${f40.modelo}`);
+console.log(f40.acelerar());
+console.log(f40.frear());
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -49,3 +49,47 @@ const celular2 = new Produto('Samsung', 10, 0)
 celular2.desconto = 0.06
 console.log(celular2.resumo())
 
+class Carro {
+    private  velocidadeAtual: number = 0
+    constructor(public marca: string, public modelo: string,
+        private velocidadeMaxima: number = 200) {
+            
+        }
+
+        protected alterarVelocidade(delta: number): number {
+            const novaVelocidade = this.velocidadeAtual + delta
+            const velocidadeValida = novaVelocidade >= 0 
+            && novaVelocidade <= this.velocidadeMaxima
+
+            if(velocidadeValida) {
+                this.velocidadeAtual = novaVelocidade
+            }else {
+                this.velocidadeAtual = delta >0 ? this.velocidadeMaxima: 0
+            }
+            return this.velocidadeAtual
+        }
+
+        public acelerar(): number {
+            return this.alterarVelocidade(5)
+        }
+
+        public frear(): number {
+            return this.alterarVelocidade(-5)
+        }
+
+}
+
+const carro1 = new Carro('For', 'Ka', 185)
+// console.log(carro1.acelerar())
+// console.log(carro1.acelerar())
+// console.log(carro1.acelerar())
+
+// Henrança
+class Ferrari extends Carro {
+
+}
+
+const f40 = new Ferrari('ferrari', 'f40', 324)
+console.log(`${f40.marca} ${f40.modelo}`)
+console.log(f40.acelerar())
+console.log(f40.frear())
