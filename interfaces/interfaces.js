29a35e2c06ed508c5cf08d7886d4a0d6efd2fe83
Bruno Henrique <b.henrique261991@gmail.com>
2022-01-16 19:48:15 -0200

    Interfaces, usando classes

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -7,9 +7,30 @@ function mudarNome(pessoa) {
 }
 const pessoa = {
     nome: 'Jõao',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
-saudarComOla({ nome: 'Jonas', idade: 27, altura: 175 });
+// saudarComOla({nome: 'Jonas', idade:27, altura: 175})
+pessoa.saudar('Bruno');
+// Usando classes ..
+class Cliente {
+    constructor() {
+        this.nome = '';
+        this.ultimaCompra = new Date;
+    }
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
+}
+const meuCliente = new Cliente();
+meuCliente.nome = 'Han';
+saudarComOla(meuCliente);
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -2,6 +2,7 @@ interface Humano {
     nome: string
     idade?: number
     [prop: string]: any
+    saudar?(sobrenome: string): void
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -13,11 +14,32 @@ function mudarNome (pessoa: Humano) {
 
 }
 
-const pessoa = {
+const pessoa: Humano = {
     nome: 'Jõao',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome: string) {
+        console.log('Olá, meu nome é '
+        + this.nome + ' ' + sobrenome)
+    }
 }
 
 saudarComOla(pessoa)
 mudarNome(pessoa)
-saudarComOla({nome: 'Jonas', idade:27, altura: 175})
+// saudarComOla({nome: 'Jonas', idade:27, altura: 175})
+pessoa.saudar('Bruno')
+
+// Usando classes ..
+class Cliente implements Humano {
+    nome: string = ''
+    ultimaCompra: Date = new Date
+    saudar(sobrenome: string) {
+        console.log('Olá, meu nome é '
+        + this.nome + ' ' + sobrenome)
+    }
+}
+
+const meuCliente = new Cliente()
+meuCliente.nome = 'Han'
+saudarComOla(meuCliente)
+meuCliente.saudar('Solo')
+console.log(meuCliente.ultimaCompra)
